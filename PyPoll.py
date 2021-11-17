632e87647d93fd696bc910dcc0473b956aa3644b
Jia Lu <jiajaja@jias-mbp.lan>
2021-11-17 15:04:22 -0800

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -13,7 +13,10 @@ total_votes = 0
 candidate_options = []
 # Declare the empty dictionary.
 candidate_votes = {}
-
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 # Open the election results and read the file
 with open(file_to_load) as election_data:
 
@@ -42,5 +45,30 @@ with open(file_to_load) as election_data:
         # Add a vote to that candidate's count (indend: align with the if statement)
         candidate_votes[candidate_name] += 1
 
-# Print the candidate list.
-print(candidate_votes)
+        # Determine the percentage of votes for each candidate by looping through the counts.
+        # 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrive vote count of a candidate
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage votes
+    vote_percentage = float(votes)/float(total_votes) * 100
+    # 4. Print the candidate name, vote count, and percentage of votes.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,}\n")
+
+    # To do: print out each candidate's name, vote count, and percentage of votes to the terminal.
+    # Determine winning vote count and candidate
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true then set winning_cout = votes and winning_percent = vote_percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # And, set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+
+winning_candidate_summary = (
+    f"--------------------------\n"
+    f"Winner:{winning_candidate}\n"
+    f"Winning Vote Count:{winning_count:,}\n"
+    f"Winning Percentage:{winning_percentage:.1f}%\n"
+    f"---------------------------\n")
+
+print(winning_candidate_summary)
