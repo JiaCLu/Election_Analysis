61baafd1b863e52f73fd4c6bc8fc6cf6b540e992
Jia Chuan Lu <92540198+bobocomfy@users.noreply.github.com>
2021-11-17 16:54:38 -0800

    Update Loop.py

--- Loop.py
+++ Loop.py
@@ -1,7 +1,3 @@
-#count = 7
-#while count < 1:
-    #print('Hello World')
-
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-outfile = open(file_to_save, "w")
-outfile.close()
+count = 7
+while count < 1:
+    print('Hello World')
