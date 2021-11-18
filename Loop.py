5e6f6131f5fcffab46156cd78a7edd0532d762eb
Jia Lu <jiajaja@jias-mbp.lan>
2021-11-17 16:52:14 -0800

    Adding the election analysis.

--- Loop.py
+++ Loop.py
@@ -1,3 +1,7 @@
-count = 7
-while count < 1:
-    print('Hello World')
+#count = 7
+#while count < 1:
+    #print('Hello World')
+
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+outfile = open(file_to_save, "w")
+outfile.close()
